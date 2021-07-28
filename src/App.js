f6931eed04b48cce6fbbf6299c41fcd043c2c889
Antonio Gonzalez <goantonio31@outlook.com>
2021-07-28 15:22:24 -0700

    completed styling for the navbar and mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>Portfolio</h1>
+    <div>
+      <Navbar />
+
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Antonio Gonzalez</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="#">About Me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>    
+    )
+}
+
+export default Navbar;
