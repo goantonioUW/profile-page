68dd2003278d6f0ac607941b45918818f878085f
Antonio Gonzalez <goantonio31@outlook.com>
2021-07-28 16:48:05 -0700

    updated the header added a background image and installed and added particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <div>
       <Navbar />
-
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "triangle",
+              stroke: {
+                width: 6,
+                color: "BE00FE"
+              }
+            }
+          }
+        }}
+      />
+      <Header />
     </div>
   );
 }
